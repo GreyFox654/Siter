1fc0fdbf421e667cac69af79a49202d67393e937
GreyFox654 <119053668+GreyFox654@users.noreply.github.com>
2022-12-04 22:20:05 +0500

    tester
    tester

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const express = require("express");
+ 
 const app = express();
 const urlencodedParser = express.urlencoded({extended: false});
  
@@ -7,27 +8,32 @@ const pool = mysql.createPool({
   connectionLimit: 5,
   host: "localhost",
   user: "root",
-  database: "saitp",
-  password: "1111"
+  database: "usersdb2",
+  password: "123456"
 });
-app.set("view engine", "html");
+ 
+app.set("view engine", "hbs");
+ 
 // получение списка пользователей
 app.get("/", function(req, res){
-    pool.query("SELECT * FROM info", function(err, data) {
+    pool.query("SELECT * FROM users", function(err, data) {
       if(err) return console.log(err);
-      res.render("index.html", {
-          info: data
+      res.render("index.hbs", {
+          users: data
       });
     });
+});
+// возвращаем форму для добавления данных
+app.get("/create", function(req, res){
+    res.render("create.hbs");
 });
 // получаем отправленные данные и добавляем их в БД 
 app.post("/create", urlencodedParser, function (req, res) {
          
     if(!req.body) return res.sendStatus(400);
-    const YorName = req.body.YorName;
-    const YorEmail = req.body.YorEmail;
-    const YorTelephone = req.body.YorTelephone;
-    pool.query("INSERT INTO info (YorName, YorEmail, YorTelephone) VALUES (?,?)", [YorName, YorEmail, YorTelephone, id], function(err, data) {
+    const name = req.body.name;
+    const age = req.body.age;
+    pool.query("INSERT INTO users (name, age) VALUES (?,?)", [name, age], function(err, data) {
       if(err) return console.log(err);
       res.redirect("/");
     });
@@ -36,10 +42,10 @@ app.post("/create", urlencodedParser, function (req, res) {
 // получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
 app.get("/edit/:id", function(req, res){
   const id = req.params.id;
-  pool.query("SELECT * FROM info WHERE id=?", [id], function(err, data) {
+  pool.query("SELECT * FROM users WHERE id=?", [id], function(err, data) {
     if(err) return console.log(err);
      res.render("edit.hbs", {
-        info: data[0]
+        user: data[0]
     });
   });
 });
@@ -47,16 +53,26 @@ app.get("/edit/:id", function(req, res){
 app.post("/edit", urlencodedParser, function (req, res) {
          
   if(!req.body) return res.sendStatus(400);
-  const YorName = req.body.name;
-  const YorEmail = req.body.age;
-  const YorTelephone = req.body.age;
+  const name = req.body.name;
+  const age = req.body.age;
   const id = req.body.id;
    
-  pool.query("UPDATE info SET YorName=?, YorEmail=?, YorTelephone=?, WHERE id=?", [YorName, YorEmail, YorTelephone, id], function(err, data) {
+  pool.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
     if(err) return console.log(err);
     res.redirect("/");
   });
 });
+ 
+// получаем id удаляемого пользователя и удаляем его из бд
+app.post("/delete/:id", function(req, res){
+          
+  const id = req.params.id;
+  pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
 app.listen(3000, function(){
-  console.log("Успешно!");
+  console.log("Сервер ожидает подключения...");
 });
